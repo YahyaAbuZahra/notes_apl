d1d218bdfab1f7ab6a1227401a64f96b45d29be9
YahyaAbuZahra GitHub <sandeep.d@simplilearn.net>
2025-02-28 10:35:19 +0300

    changes theme and create notes view

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:notes_apl/constants.dart';
+import 'package:notes_apl/cubits/add_note_cubit/add_note_cubit.dart';
+import 'package:notes_apl/models/note_model.dart';
+import 'package:notes_apl/simple_bloc_observer.dart';
 
-void main() {
+import 'package:notes_apl/views/notes_view.dart';
+
+import 'cubits/notes_cubit/notes_cubit.dart';
+
+void main() async {
+  await Hive.initFlutter();
+
+  Bloc.observer = SimpleBlocObserver();
+  Hive.registerAdapter(NoteModelAdapter());
+  await Hive.openBox<NoteModel>(kNotesBox);
+
   runApp(const NotesApp());
 }
 
 class NotesApp extends StatelessWidget {
-  const NotesApp({super.key});
+  const NotesApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return BlocProvider(
+      create: (context) => NotesCubit(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(brightness: Brightness.dark, fontFamily: 'Poppins'),
+        home: const NotesView(),
+      ),
+    );
   }
 }
